56e041f8e933e09dd8bef9815c3a14c14ec79d92
brenton ferullo <brenton.ferullo@me.com>
2021-05-26 00:41:28 -0400

    added offset support

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -15,10 +15,13 @@ const head_cell_color_style = {
     color: "white",
     padding: "7px",
 }
+const menu_button_style = {
+    whiteSpace: "nowrap"
+}
 //end styles
 
-
 const RANGE_NAME = "quiz_range";
+const OFFSET_NAME = "quiz_offset";
 const BEGIN_QUIZ_NAME = "begin_quiz";//takes you to options menu
 const START_QUIZ_NAME = "start_quiz";//acutally starts quiz
 const END_QUIZ_NAME = "end_quiz";
@@ -42,33 +45,26 @@ function Quiz(props) {
     }
 
     const [q_range, set_q_range] = React.useState(20);
+    const [q_range_offset, set_q_range_offset] = React.useState(0);
     const [q_page, set_q_page] = React.useState(1);
     const [q_table, set_q_table] = React.useState(makeTable());
     const [q_selection_error, set_q_selection_error] = React.useState(false);
     const [q_questions, set_q_questions] = React.useState([]);
     const [q_current_question, set_q_current_question] = React.useState(0);
     const [q_incorrect_counter, set_q_incorrect_counter] = React.useState(0);
-
-    const DEFAULT_SELECTION = keys()[0]
-
     const [q_selected_question, set_q_selected_question] = React.useState(keys()[0]);
     const [q_selected_answer, set_q_selected_answer] = React.useState(keys()[0]);
     const [q_answered_counter, set_q_answered_counter] = React.useState(0);
     const [q_answers, set_q_answers] = React.useState(4);
-
     const [q_chosen_quiz, set_chosen_quiz] = React.useState(0);
     const [q_is_shown, set_q_stage] = React.useState(TABLESTAGE); 
 
-    function handleSelectionChange(event) {
-      console.log(event.target.value)
-      set_chosen_quiz(Number(event.target.value));
-      
+    function q_length() {
+        return q_data.length;
     }
 
-    let q_length = q_data.length;
-
     function page_count() {
-        return Math.ceil(q_length / q_range);
+        return Math.ceil(q_length() / q_range);
     }
 
     function input_box_width(basis) {
@@ -83,57 +79,9 @@ function Quiz(props) {
             q_range * q_page
         ]
 
+        range = range.map(x => x + q_range_offset);
+
         return q_data.slice(range[0], range[1])
-    }
-
-    function change_q_answers(value) {
-        if (value < 0) {
-            value = 0;
-        }
-
-        let len = selected_items().length
-
-        if (value > len) {
-            value = len
-        }
-
-        set_q_answers(value)
-    }
-
-    function change_current_question() {
-        let m = (selected_items().length - 1) - q_answered_counter, i=0;
-
-        console.log('m', m);
-        
-        if (m > 0) {
-            i = Math.floor(Math.random() * m);
-        }
-
-        set_q_current_question(i);
-    }
-
-    function change_page(a) {
-        if (a < 0) {
-            a = page_count();
-        }
-
-        if (a > page_count()) {
-            a = 0;
-        }
-
-        set_q_page(a);
-    }
-
-    function change_range(a) {
-        if (a < 0) {
-            a = q_length;
-        }
-
-        if (a > q_length) {
-            a = 0;
-        }
-
-        set_q_range(a);
     }
 
     function random_answers(arr, count) {
@@ -170,119 +118,7 @@ function Quiz(props) {
         return arr;
     }
 
-    function quiz_select() {
-        return (<>
-            Choose Quiz: <select name={QUIZ_SELECT_NAME} value={q_chosen_quiz} onChange={handleEvent}>
-                {props.datasets.map((x,i) => {
-                return (
-                    <option value={i}>{x.title}</option>
-                )
-                })}
-            </select>
-        </>)
-    }
-
-    function resetQuiz() {
-        change_current_question()
-        set_q_incorrect_counter(0);
-        set_q_answered_counter(0);
-    }
-
-    function quiz_modal() {
-        let err = "";
-        let data = <></>;
-
-        if (q_selection_error) {
-            err = "error";
-        }
-
-        switch (q_is_shown) {
-            case TABLESTAGE:
-                data = q_table;
-                break;
-            case OPTIONSSTAGE:
-                data = (
-                    <div>
-                        <button name={END_QUIZ_NAME} onClick={handleEvent} >End Quiz</button><br />
-                        Question: <select value={q_selected_question} onChange={handleEvent} name={QUESTION_SELECT_NAME}>
-                            {keys().map(x => {
-                                return <option value={x}>{x}</option>
-                            })}
-                        </select>
-                        <br />
-                        Answer: <select value={q_selected_answer} onChange={handleEvent} name={ANSWER_SELECT_NAME}>
-                            {keys().map(x => {
-                                return <option value={x}>{x}</option>
-                            })}
-                        </select>
-                        <br />
-                        <input style={input_box_width(q_answers)} type="number" value={q_answers} name={NUMBER_OF_ANSWERS_NAME} onChange={handleEvent} />
-                        <br />
-                        <button onClick={handleEvent} name={START_QUIZ_NAME}>Begin Quiz</button><p>{err}</p>
-                    </div>
-                )
-                break;
-            case QUIZSTAGE:
-                console.log("q_questions", q_questions);
-                let question = q_questions[q_current_question].question;
-                let answers = shuffle(random_answers(q_questions, q_answers).slice());
-
-                data = (
-                    <div>
-                        <b>Q: {question}</b>
-                        <br /><br />
-                        {answers.map((x,i) => (
-                            <>{`${i+1}:\t\t`}<button onClick={handleEvent} data-question={x.toString()} value={i} name={ANSWER_BUTTON_NAME}>{x}</button><br /></>
-                        ))}
-                        <br />Correct Answers: {q_answered_counter}
-                        <br />Incorrect Answers: {q_incorrect_counter}
-                        <br /><button onClick={handleEvent} name={END_QUIZ_NAME}>End Quiz</button>
-                    </div>
-                )
-                break;
-            case SCORECARDSTAGE:
-                data = (
-                    <div>
-                        You Got {q_answered_counter} correct and {q_incorrect_counter} incorrect.<br />
-                        You scored {selected_items().length - q_incorrect_counter}
-                        <br />
-                        <button onClick={handleEvent} name={END_QUIZ_NAME}>End Quiz</button>
-                        <button onClick={handleEvent} name={RETAKE_NAME}>Retake Quiz</button>
-                        <br />What you need to study:<br />
-                        <table style={table_style}>
-                            <thead>
-                                <tr>
-                                {[...Object.keys(q_questions[0])].map(x => {
-                                    return (
-                                        <td style={{...cell_style,...head_cell_color_style}}>{x}</td>
-                                    )
-                                })}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {q_questions.filter(x => x.fails > 0).map(x => {
-                                    return (
-                                        <tr>
-                                            {[...Object.keys(x)].map(xx => {
-                                                return <td style={cell_style}>{x[xx]}</td>
-                                            })}
-                                        </tr>
-                                        
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                        
-                    </div>
-                )
-                break;
-            default:
-                data = <b>sumting broked</b>
-                break;
-        }
-        return data;
-    }
-
+    //EVENTS
     function handleEvent(event) {
         set_q_selection_error(false);
         let name = event.target.name;
@@ -296,6 +132,9 @@ function Quiz(props) {
             case RANGE_NAME:
                 change_range(value);
                 set_q_questions(selected_items());
+                break;
+            case OFFSET_NAME:
+                change_offset(value);
                 break;
             case BEGIN_QUIZ_NAME:
                 if (selected_items().length > 0) {
@@ -337,7 +176,6 @@ function Quiz(props) {
                 answer_button_event(event);
                 break;
             case RETAKE_NAME:
-
                 set_q_stage(OPTIONSSTAGE);
                 resetQuiz();
                 break;
@@ -353,7 +191,7 @@ function Quiz(props) {
         }
         event.preventDefault();
     }
-
+    
     function answer_button_event(event) {
         if (event.target.dataset.question === q_questions[q_current_question].answer) {//correct answer
             let arr = q_questions.slice();
@@ -384,10 +222,83 @@ function Quiz(props) {
         set_q_table(makeTable())
         event.preventDefault();
     }
+    //END EVENTS
 
+    //STATE CHANGERS
+    function handleSelectionChange(event) {
+        set_chosen_quiz(Number(event.target.value));
+    }
+
+    function resetQuiz() {
+        change_current_question()
+        set_q_incorrect_counter(0);
+        set_q_answered_counter(0);
+    }
+
+    function change_offset(a) {
+        if (a < 0) {
+            a = 0
+        }
+
+        set_q_range_offset(a);
+    }
+
+    function change_current_question() {
+        let m = (selected_items().length - 1) - q_answered_counter, i=0;
+
+        console.log('m', m);
+        
+        if (m > 0) {
+            i = Math.floor(Math.random() * m);
+        }
+
+        set_q_current_question(i);
+    }
+
+    function change_page(a) {
+        if (a < 0) {
+            a = page_count();
+        }
+
+        if (a > page_count()) {
+            a = 0;
+        }
+
+        set_q_page(a);
+    }
+
+    function change_range(a) {
+        if (a < 0) {
+            a = q_length();
+        }
+
+        if (a > q_length()) {
+            a = 0;
+        }
+
+        set_q_range(a);
+    }
+    
+    function change_q_answers(value) {
+        if (value < 0) {
+            value = 0;
+        }
+
+        let len = selected_items().length
+
+        if (value > len) {
+            value = len
+        }
+
+        set_q_answers(value)
+    }
+    //END STATE CHANGERS
+
+    //JSX ELEMENTS
     let q_range_form = (
         <form onSubmit={handleRangeFormSubmit}>
-            Quiz Size:<br/><input name={RANGE_NAME} style={input_box_width(q_range)} value={q_range.toString()} type="number" onChange={handleEvent}/>/{q_length}<br/>
+            Quiz Size:<br/><input name={RANGE_NAME} style={input_box_width(q_range)} value={q_range.toString()} type="number" onChange={handleEvent}/>/{q_length()}<br/>
+            Offset: <br /><input name={OFFSET_NAME} style={input_box_width(q_range_offset)} value={q_range_offset.toString()} type="number" onChange={handleEvent} /><br/>
             {/*<input type="submit"/>*/}
         </form>
     )
@@ -399,10 +310,10 @@ function Quiz(props) {
         }
 
         let cells = [
-            <button onClick={() => page_increment(-1)} >{"Previous Page"}</button>,
-            <button onClick={() => page_increment(1)} >{"Next Page"}</button>,
+            <button style={menu_button_style} onClick={() => page_increment(-1)} >{"Previous Page"}</button>,
+            <button style={menu_button_style} onClick={() => page_increment(1)} >{"Next Page"}</button>,
             <><input style={input_box_width(q_page)} type="number" name={PAGE_NAME} onChange={handleEvent} value={q_page.toString()} />{`/${page_count()}`}</>,
-            <button name={BEGIN_QUIZ_NAME} onClick={handleEvent}>{"Begin Quiz"}</button>
+            <button style={menu_button_style} name={BEGIN_QUIZ_NAME} onClick={handleEvent}>{"Begin Quiz"}</button>
         ];
 
 
@@ -460,8 +371,118 @@ function Quiz(props) {
             </div>
         )
     }
+
+    function quiz_select() {
+        return (<>
+            Choose Quiz: <select name={QUIZ_SELECT_NAME} value={q_chosen_quiz} onChange={handleEvent}>
+                {props.datasets.map((x,i) => {
+                return (
+                    <option value={i}>{x.title}</option>
+                )
+                })}
+            </select>
+        </>)
+    }
+
+    function quiz_modal() {
+        let err = "";
+        let data = <></>;
+
+        if (q_selection_error) {
+            err = "error";
+        }
+
+        switch (q_is_shown) {
+            case TABLESTAGE:
+                data = q_table;
+                break;
+            case OPTIONSSTAGE:
+                data = (
+                    <div>
+                        <button style={menu_button_style} name={END_QUIZ_NAME} onClick={handleEvent} >End Quiz</button><br />
+                        Question: <select value={q_selected_question} onChange={handleEvent} name={QUESTION_SELECT_NAME}>
+                            {keys().map(x => {
+                                return <option value={x}>{x}</option>
+                            })}
+                        </select>
+                        <br />
+                        Answer: <select value={q_selected_answer} onChange={handleEvent} name={ANSWER_SELECT_NAME}>
+                            {keys().map(x => {
+                                return <option value={x}>{x}</option>
+                            })}
+                        </select>
+                        <br />
+                        <input style={input_box_width(q_answers)} type="number" value={q_answers} name={NUMBER_OF_ANSWERS_NAME} onChange={handleEvent} />
+                        <br />
+                        <button style={menu_button_style} onClick={handleEvent} name={START_QUIZ_NAME}>Begin Quiz</button><p>{err}</p>
+                    </div>
+                )
+                break;
+            case QUIZSTAGE:
+                console.log("q_questions", q_questions);
+                let question = q_questions[q_current_question].question;
+                let answers = shuffle(random_answers(q_questions, q_answers).slice());
+
+                data = (
+                    <div>
+                        <b>Q: {question}</b>
+                        <br /><br />
+                        {answers.map((x,i) => (
+                            <>{`${i+1}:\t\t`}<button onClick={handleEvent} data-question={x.toString()} value={i} name={ANSWER_BUTTON_NAME}>{x}</button><br /></>
+                        ))}
+                        <br />Questions Left: {q_range - q_answered_counter}
+                        <br />Correct Answers: {q_answered_counter}
+                        <br />Incorrect Answers: {q_incorrect_counter}
+                        <br /><button style={menu_button_style} onClick={handleEvent} name={END_QUIZ_NAME}>End Quiz</button>
+                    </div>
+                )
+                break;
+            case SCORECARDSTAGE:
+                data = (
+                    <div>
+                        You Got {q_answered_counter} correct and {q_incorrect_counter} incorrect.<br />
+                        You scored {selected_items().length - q_incorrect_counter}
+                        <br />
+                        <button style={menu_button_style} onClick={handleEvent} name={END_QUIZ_NAME}>End Quiz</button>
+                        <button style={menu_button_style} onClick={handleEvent} name={RETAKE_NAME}>Retake Quiz</button>
+                        <br />What you need to study:<br />
+                        <table style={table_style}>
+                            <thead>
+                                <tr>
+                                {[...Object.keys(q_questions[0])].map(x => {
+                                    return (
+                                        <td style={{...cell_style,...head_cell_color_style}}>{x}</td>
+                                    )
+                                })}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {q_questions.filter(x => x.fails > 0).map(x => {
+                                    return (
+                                        <tr>
+                                            {[...Object.keys(x)].map(xx => {
+                                                return <td style={cell_style}>{x[xx]}</td>
+                                            })}
+                                        </tr>
+                                        
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                        
+                    </div>
+                )
+                break;
+            default:
+                data = <b>sumting broked</b>
+                break;
+        }
+        return data;
+    }
+    //END JSX ELEMENTS
     
-    React.useEffect(() => set_q_table(makeTable()), [q_page, q_range])
+    //REACT EFFECTS
+    React.useEffect(() => set_q_table(makeTable()), [q_page, q_range, q_range_offset])
     React.useEffect(() => {
         set_q_table(makeTable());
         set_q_selected_question(keys()[props.datasets[q_chosen_quiz].default_qa[0]]);
@@ -470,6 +491,7 @@ function Quiz(props) {
     React.useEffect(() => {
         set_q_data(props.datasets[q_chosen_quiz].data);
     }, [q_chosen_quiz]);
+    //END REACT EFFECTS
 
     return (
         <div>
